6ad9c8b810fb3988deb18e56d9bc754036b04b58
Ceping Sun <cepingx.sun@intel.com>
2023-08-10 03:35:32 -0400

    SecurityPkg/TdTcg2Dxe: Detect measurement type at the start
    Signed-off-by: Ceping Sun <cepingx.sun@intel.com>

--- SecurityPkg/Tcg/TdTcg2Dxe/TdTcg2Dxe.c
+++ SecurityPkg/Tcg/TdTcg2Dxe/TdTcg2Dxe.c
@@ -2431,6 +2431,25 @@ DriverEntry (
 
   mImageHandle = ImageHandle;
 
+#ifdef VTPM_FEATURE_ENABLED
+  EDKII_VTPM_BASED_MEASUREMENT_PROTOCOL *VtpmBasedMeasurementProtocol;
+
+  Status = gBS->LocateProtocol(&gEdkiiVtpmBasedMeasurementProtocolGuid, NULL, (VOID **)&VtpmBasedMeasurementProtocol);
+  if (EFI_ERROR(Status))
+  {
+      DEBUG((DEBUG_ERROR, "Locate VtpmBasedMeasurementProtocol failed with %r\n", Status));
+      ASSERT(FALSE);
+      return Status;
+  }
+
+  Status = VtpmBasedMeasurementProtocol->VtpmDetect(VtpmBasedMeasurementProtocol);
+  if (Status == EFI_SUCCESS)
+  {
+    DEBUG ((DEBUG_INFO, "It is vTPM based measurement.\n"));
+    return EFI_UNSUPPORTED;
+  }
+#endif
+
   //
   // Fill information
   //
