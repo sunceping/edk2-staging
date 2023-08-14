26b39d7762d62027bb446cd6dc3288b78f1c0f83
Ceping Sun <cepingx.sun@intel.com>
2023-08-13 21:22:52 -0400

    OvmfPkg/VmmSpdmVTpm: Add VtpmClearSharedBuffer in Dxe phase
    Signed-off-by: Ceping Sun <cepingx.sun@intel.com>

--- OvmfPkg/Library/VmmSpdmVTpm/VmmSpdmInternal.h
+++ OvmfPkg/Library/VmmSpdmVTpm/VmmSpdmInternal.h
@@ -188,6 +188,11 @@ VtpmAllocateSharedBuffer(
   IN UINT32      Pages
 );
 
+EFI_STATUS
+VtpmClearSharedBuffer (
+  VOID
+  );
+
 VTPM_SECURE_SESSION_INFO_TABLE *
 GetSpdmSecuredSessionInfo (
   VOID
